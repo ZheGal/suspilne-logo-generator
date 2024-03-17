a9bf89e2d36ea85159699a4df45ffca97cad3200
Evgeniy Galkovskiy <zhegal@gmail.com>
2024-03-17 19:33:42 +0200

    Logo generator

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container mx-auto py-4">
+    <h1 class="text-center text-xl mb-4 font-black">
+      Генератор логотипів "Суспільного"
+    </h1>
+    <div class="main-form">
+      <label class="main-label">
+        <div class="text-right w-[120px]">Перший рядок:</div>
+        <input type="text" v-model="firstLine" class="main-input" />
+      </label>
+      <label class="main-label">
+        <div class="text-right w-[120px]">Другий рядок:</div>
+        <input type="text" v-model="secondLine" class="main-input" />
+      </label>
+      <label class="main-label">
+        <div class="text-right w-[120px]">Колір:</div>
+        <input type="color" v-model="color" />
+      </label>
+    </div>
+    <hr class="my-4 border-black/30" />
+    <Logo :firstLine="firstLine" :secondLine="secondLine" :roundColor="color" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Logo from "./components/Logo.vue";
+
+const firstLine = ref<string>("Суспільне");
+const secondLine = ref<string>("Новини");
+const color = ref<string>("#FBED21");
+</script>
+
+<style lang="css" scoped>
+.main-form {
+  @apply flex flex-col gap-1 container max-w-[350px] mx-auto;
+}
+.main-label {
+  @apply flex flex-row gap-4 w-full;
+}
+.main-input {
+  @apply border border-black px-1 flex-1 rounded-md;
+}
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="logo">
+    <div class="logo_inner">
+      <div class="logo_round"></div>
+      <div class="logo_text">
+        <div v-if="firstLine">{{ firstLine }}</div>
+        <div v-if="secondLine">{{ secondLine }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  firstLine?: string;
+  secondLine?: string;
+  roundColor?: string;
+}>();
+</script>
+
+<style lang="css" scoped>
+.logo {
+  @apply flex items-center justify-center;
+  @apply font-['Inerta'] font-black py-12 px-2;
+  @apply rounded-xl border bg-slate-700 text-white uppercase;
+  @apply pointer-events-none select-none;
+}
+
+.logo_inner {
+  @apply flex flex-row items-center justify-center gap-[30px];
+  @apply max-sm:scale-50;
+}
+
+.logo_round {
+  @apply aspect-[1/2] min-w-[60px] max-w-[60px] bg-[v-bind(roundColor)] rounded-l-full;
+}
+
+.logo_text {
+  @apply flex flex-col leading-[54px] font-bold text-white text-[52px];
+}
+</style>
